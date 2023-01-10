e033f927c88d612701ecd45a4b72655a423bbe4f
CocoS26 <Coco.Shen@KPMG.co.uk>
2023-01-10 09:14:42 +0000

    added categories

--- models/model.js
+++ models/model.js
@@ -12,7 +12,7 @@ selectCategories = () => {
 selectReviews = (category, sort_by= 'created_at', order='desc') => {
 
     const validSortedByQueries = ['review_id', 'title', 'category', 'designer', 'owner', 'review_img_url', 'created_at' ] 
-    const validCategories = [ 'euro game','social deduction','dexterity',"children's games"]
+    const validCategories = [ 'euro game','social deduction','dexterity',"children's games","strategy", "hidden-roles", "dexterity", "push-your-luck", "roll-and-write", "deck-building", "engine-building"]
     const validOrderQueries = ['asc', 'desc']
     
     if (!validSortedByQueries.includes(sort_by)) {
